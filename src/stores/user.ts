a0dffa94da4c830ff7a28299f6709d6efe682f8b
felipe <felipe_lima10@icloud.com>
2023-04-02 20:48:33 -0300

    feat: add doc in userCreateUser

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,23 +1,24 @@
 import { IAuth, IInfo, IUser } from '@services/firebase/models/user';
 import { create } from 'zustand';
 
+export interface ICreatedUser extends Partial<IInfo> {
+  doc?: string;
+}
+
 type State = {
   user: Partial<IUser> | null;
   auth: (auth: IAuth) => void;
   login: (user: IUser) => void;
   logout: () => void;
-
-  userCreate: Partial<IInfo> | null;
-  setCreateUser: (values: Partial<IInfo>) => void;
+  userCreate: Partial<ICreatedUser> | null;
+  setCreateUser: (values: Partial<ICreatedUser>) => void;
 };
 
 export const useUserStore = create<State>(set => ({
   user: null,
-  auth: ({ name, email, lastName }) =>
-    set(() => ({ user: { name, email, lastName } })),
+  auth: values => set(() => ({ user: { ...values } })),
   login: (user: IUser) => set(() => ({ user })),
   logout: () => set(() => ({ user: null })),
-
   userCreate: null,
   setCreateUser: values =>
     set(state => ({ userCreate: { ...state.userCreate, ...values } })),
