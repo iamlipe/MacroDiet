efc0b738c5879dfea5635d28a8c763364804e8f6
felipe <felipe_lima10@icloud.com>
2023-04-06 20:17:56 -0300

    fix: measures mock

--- __mocks__/measures.ts
+++ __mocks__/measures.ts
@@ -1,35 +1,31 @@
 import { IMeasure } from '@services/firebase/models/measure';
 
 interface MeasuresData {
-  data: {
-    mass: IMeasure[];
-    length: IMeasure[];
-  };
+  data: Array<IMeasure>;
 }
 
 export const measures: MeasuresData = {
-  data: {
-    mass: [
-      {
-        doc: '1',
-        acronym: 'g',
-        title: 'gram',
-        multiple: 1,
-      },
-      {
-        doc: '2',
-        acronym: 'kg',
-        title: 'kilograma',
-        multiple: 1000,
-      },
-    ],
-    length: [
-      {
-        doc: '3',
-        acronym: 'cm',
-        title: 'centimeter',
-        multiple: 1,
-      },
-    ],
-  },
+  data: [
+    {
+      doc: '1',
+      acronym: 'g',
+      title: 'gram',
+      multiple: 1,
+      type: 'mass',
+    },
+    {
+      doc: '2',
+      acronym: 'kg',
+      title: 'kilograma',
+      multiple: 1000,
+      type: 'mass',
+    },
+    {
+      doc: '3',
+      acronym: 'cm',
+      title: 'centimeter',
+      multiple: 1,
+      type: 'length',
+    },
+  ],
 };
