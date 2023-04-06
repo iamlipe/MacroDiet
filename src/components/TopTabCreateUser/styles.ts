461601a3deb4e163c8b0b62817e5977fbe0e5f31
felipe <felipe_lima10@icloud.com>
2023-04-06 00:30:36 -0300

    fix: progress bar

--- src/components/TopTabCreateUser/styles.ts
+++ src/components/TopTabCreateUser/styles.ts
@@ -1,38 +1,22 @@
 import styled from 'styled-components/native';
 
-interface Progressbar {
-  progress: number;
-}
-
-export const Wrapper = styled.SafeAreaView`
-  background-color: ${({ theme }) => theme.colors.background.dark};
-`;
-
-export const Container = styled.View`
-  width: 100%;
+export const StyledContainer = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   padding: ${({ theme }) =>
     `${theme.effects.spacing.hg}px ${theme.effects.spacing.md}px`};
+  background-color: ${({ theme }) => theme.colors.background.dark};
 `;
 
-export const ContainerProgressbar = styled.View`
-  height: 12px;
-  width: 80%;
-  border-radius: ${({ theme }) => theme.effects.border.radius.pill}px;
-  background-color: ${({ theme }) => theme.colors.primary[200]};
-`;
-
-export const Progressbar = styled.View<Progressbar>`
-  position: absolute;
-  width: ${({ progress }) => progress}%;
-  height: 12px;
-  background-color: ${({ theme }) => theme.colors.primary[500]};
-  border-radius: ${({ theme }) => theme.effects.border.radius.pill}px;
-`;
-
-export const BaseButton = styled.TouchableOpacity`
+export const StyledBaseButton = styled.TouchableOpacity`
   width: 24px;
   height: 24px;
+`;
+
+export const StyledWrapperProgressbar = styled.View`
+  flex: 1;
+  padding: 0 ${({ theme }) => theme.effects.spacing.vl}px;
+  align-items: center;
+  justify-content: space-between;
 `;
