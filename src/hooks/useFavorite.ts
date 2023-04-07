de24833ede7fc8ea93652efd0ac83546b860899a
felipe <felipe_lima10@icloud.com>
2023-04-06 21:32:06 -0300

    feat: update food in firebase done

--- src/hooks/useFavorite.ts
+++ src/hooks/useFavorite.ts
@@ -1,26 +1,54 @@
 import { useUserStore } from '@stores/user';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useFoods } from './useFoods';
+import { useHandleError } from './useHandleError';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 export const useFavorite = () => {
-  const [favoritesFoods, setFavoritesFoods] = useState([]);
-  const { user } = useUserStore();
+  const [loading, setLoading] = useState(false);
+  const [favoritesFoodsList, setFavoritesFoodsList] = useState([]);
+  const { handleFirestoreError } = useHandleError();
+  const { user, setFavoritesFoods } = useUserStore();
   const { getFood } = useFoods();
 
-  const handleFavorite = useCallback(() => {}, []);
+  const updateFavoritesFoods = useCallback(
+    async (foodDoc: string) => {
+      try {
+        setLoading(true);
+
+        const isFavorited = user.preferences.favoritesFoods.includes(foodDoc);
+
+        const favorites = isFavorited
+          ? user.preferences.favoritesFoods.filter(food => food !== foodDoc)
+          : [...user.preferences.favoritesFoods, foodDoc];
+
+        await firestore()
+          .collection('Users')
+          .doc(auth().currentUser.uid)
+          .update({
+            'preferences.favoritesFoods': favorites,
+          });
+
+        setFavoritesFoods(favorites);
+      } catch (error) {
+        handleFirestoreError(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [handleFirestoreError, setFavoritesFoods, user.preferences.favoritesFoods],
+  );
 
   const getFavoritesFoods = useMemo(() => {
     return user.preferences.favoritesFoods.map(foodDoc => getFood(foodDoc));
   }, [getFood, user.preferences.favoritesFoods]);
 
-  useEffect(() => {
-    setFavoritesFoods(getFavoritesFoods);
-  }, [getFavoritesFoods]);
-
   return {
-    handleFavorite,
-    favoritesFoods,
+    favoritesFoodsList,
     getFavoritesFoods,
-    setFavoritesFoods,
+    setFavoritesFoodsList,
+    updateFavoritesFoods,
+    loading,
   };
 };
