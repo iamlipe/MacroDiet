aa79238f59709ade8a29938460149be5fb401b52
felipe <felipe_lima10@icloud.com>
2023-04-10 18:15:43 -0300

    feat: add icons left in options menu and diet up

--- src/screens/MenuProfile/index.tsx
+++ src/screens/MenuProfile/index.tsx
@@ -1,43 +1,95 @@
 import React from 'react';
 import { NavPropsLogged } from '@routes/logged';
 import { useNavigation } from '@react-navigation/native';
-import { useUserStore } from '@stores/index';
-import { useMenu } from '@hooks/index';
-import { Background, Header } from '@components/index';
+import { useLogin, useMenu } from '@hooks/index';
+import { Background, Header, Icon, Link } from '@components/index';
 import {
   StyledScroll,
   StyledCardOption,
-  StyledPhotoUser,
+  StyledContainerDietUp,
+  StyledLogo,
   StyledCircle,
-  StyledInitialLetterUser,
+  StyledLabelExplainDietUp,
+  StyledContainerOverviewDietUp,
+  StyledScore,
+  StyledContainerScore,
+  StyledDayWeek,
+  StyledContainerMenuOptions,
+  StyledVersion,
 } from './styles';
+import { useTheme } from 'styled-components/native';
+import { View } from 'react-native';
+import moment from 'moment';
 
 const MenuProfile = () => {
   const { menuOptions } = useMenu();
-  const { user } = useUserStore();
+  // const { user } = useUserStore();
+  const { logout } = useLogin();
   const { navigate: navigateProfile } = useNavigation<NavPropsLogged>();
+  const { colors, fonts } = useTheme();
 
   return (
     <Background>
       <Header title="menu" />
 
       <StyledScroll>
-        {!user.photo ? (
-          <StyledPhotoUser source={{ uri: user.photo }} />
-        ) : (
-          <StyledCircle>
-            <StyledInitialLetterUser>{user.name[0]}</StyledInitialLetterUser>
-          </StyledCircle>
-        )}
+        <StyledContainerDietUp>
+          <View>
+            <StyledLogo name="logo" size={80} color={colors.primary[500]} />
+            <StyledContainerScore>
+              <StyledScore>12</StyledScore>
+            </StyledContainerScore>
+          </View>
 
-        {menuOptions.map(option => (
-          <StyledCardOption
-            key={option.key}
-            title={option.name}
-            icon={{ name: 'right' }}
-            onPress={() => navigateProfile(option.navigateTo)}
-          />
-        ))}
+          <StyledContainerOverviewDietUp>
+            {new Array(7).fill(0).map((_, index) => {
+              const date = new Date();
+              date.setDate(date.getDay() - (6 + index) * -1);
+
+              return (
+                <View style={{ alignItems: 'center' }}>
+                  <StyledDayWeek>{moment(date).format('ddd')}</StyledDayWeek>
+
+                  <StyledCircle isDiscoloring={index === 0 || index === 6}>
+                    {index < 4 && (
+                      <Icon
+                        name="check"
+                        color={colors.primary[400]}
+                        size={fonts.size.lg}
+                      />
+                    )}
+                  </StyledCircle>
+                </View>
+              );
+            }, [])}
+          </StyledContainerOverviewDietUp>
+
+          <StyledLabelExplainDietUp>
+            Lorem Ipsum is simply dummy text of the printing and typesetting.
+          </StyledLabelExplainDietUp>
+        </StyledContainerDietUp>
+
+        <StyledContainerMenuOptions>
+          {menuOptions.map(option => (
+            <StyledCardOption
+              key={option.key}
+              title={option.name}
+              type="bottomLine"
+              iconLeft={{ name: option.iconName }}
+              icon={{ name: 'right' }}
+              onPress={() => navigateProfile(option.navigateTo)}
+            />
+          ))}
+        </StyledContainerMenuOptions>
+
+        <Link
+          title="Sair"
+          size={fonts.size.s2}
+          icon={{ name: 'logout', position: 'right' }}
+          onPress={logout}
+        />
+
+        <StyledVersion>versao 1.0.0</StyledVersion>
       </StyledScroll>
     </Background>
   );
--- src/screens/MenuProfile/styles.ts
+++ src/screens/MenuProfile/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native';
-import { Card } from '@components/index';
+import { Card, Icon } from '@components/index';
+
+interface IStyledCircle {
+  isDiscoloring: boolean;
+}
 
 export const StyledScroll = styled.ScrollView.attrs(({ theme }) => ({
   showsVerticalScrollIndicator: false,
@@ -13,28 +17,74 @@ export const StyledCardOption = styled(Card)`
   margin-bottom: ${({ theme }) => theme.effects.spacing.md}px;
 `;
 
-export const StyledPhotoUser = styled.Image`
-  height: 80px;
-  width: 80px;
-  align-self: center;
-  border-radius: ${({ theme }) => theme.effects.border.radius.circular}px;
-  background-color: ${({ theme }) => theme.colors.primary[600]};
-  margin-bottom: ${({ theme }) => theme.effects.spacing.hg}px;
-`;
-
-export const StyledCircle = styled.View`
-  height: 80px;
-  width: 80px;
-  align-self: center;
+export const StyledContainerDietUp = styled.View`
+  min-height: 280px;
   align-items: center;
   justify-content: center;
-  border-radius: ${({ theme }) => theme.effects.border.radius.circular}px;
-  background-color: ${({ theme }) => theme.colors.primary[600]};
+  padding: ${({ theme }) => theme.effects.spacing.md}px;
+  margin-bottom: ${({ theme }) => theme.effects.spacing.vl}px;
+`;
+
+export const StyledLogo = styled(Icon)`
   margin-bottom: ${({ theme }) => theme.effects.spacing.hg}px;
 `;
 
-export const StyledInitialLetterUser = styled.Text`
+export const StyledContainerOverviewDietUp = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-bottom: ${({ theme }) => theme.effects.spacing.vl}px;
+`;
+
+export const StyledCircle = styled.View<IStyledCircle>`
+  height: 24px;
+  width: 24px;
+  justify-content: center;
+  align-items: center;
+  border-radius: ${({ theme }) => theme.effects.border.radius.circular}px;
+  background-color: ${({ theme }) => theme.colors.primary[200]};
+  opacity: ${({ isDiscoloring }) => (isDiscoloring ? 0.4 : 1)};
+`;
+
+export const StyledLabelExplainDietUp = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.family.regular};
+  font-size: ${({ theme }) => theme.fonts.size.md}px;
+  color: ${({ theme }) => theme.colors.gray[200]};
+  text-align: center;
+`;
+
+export const StyledContainerScore = styled.View`
+  position: absolute;
+  top: 4;
+  left: 0;
+  height: 80;
+  width: 80;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledScore = styled.Text`
   font-family: ${({ theme }) => theme.fonts.family.bold};
   font-size: ${({ theme }) => theme.fonts.size.lt}px;
+  color: ${({ theme }) => theme.colors.primary[100]};
+`;
+
+export const StyledDayWeek = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.family.medium};
+  font-size: ${({ theme }) => theme.fonts.size.md}px;
   color: ${({ theme }) => theme.colors.white};
+  margin-bottom: ${({ theme }) => theme.effects.spacing.sm}px;
+`;
+
+export const StyledContainerMenuOptions = styled.View`
+  margin-bottom: ${({ theme }) => theme.effects.spacing.hg}px;
+`;
+
+export const StyledVersion = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.family.regular};
+  font-size: ${({ theme }) => theme.fonts.size.lg}px;
+  color: ${({ theme }) => theme.colors.gray[200]};
+  text-align: center;
+  margin-top: ${({ theme }) => 2 * theme.effects.spacing.hg}px;
 `;
