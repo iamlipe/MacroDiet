7d74744da0652fa2881d3bc036a202a6462932eb
felipe <felipe_lima10@icloud.com>
2023-04-12 16:54:21 -0300

    refactor: remove unused store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,6 @@
 export { default as useActivityStore } from './acitivity';
 export { default as useFoodStore } from './food';
 export { default as useGenderStore } from './gender';
-export { default as useGoalStore } from './goal';
 export { default as useMealStore } from './meal';
 export { default as useMeasureStore } from './measure';
 export { default as useSyncStore } from './sync';
