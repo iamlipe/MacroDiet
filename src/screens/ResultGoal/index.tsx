86a80017aa42bafd862ef165ef317c528964c72c
felipe <felipe_lima10@icloud.com>
2023-04-12 17:00:11 -0300

    feat: screen result goal done

---
+++ src/screens/ResultGoal/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { NavPropsProfile } from '@routes/profileStack';
+import { Background, Header, Button } from '@components/index';
+import {
+  StyledScroll,
+  StyledWrapperButtonSubmit,
+  StyledTitle,
+  StyledLabel,
+} from './styles';
+
+import useUserStore from '@stores/user';
+
+const ResultGoal = () => {
+  const { navigate } = useNavigation<NavPropsProfile>();
+  const { user } = useUserStore();
+
+  return (
+    <Background>
+      <Header title="Resultado" />
+
+      <StyledScroll>
+        <StyledTitle>Title</StyledTitle>
+
+        <StyledLabel>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </StyledLabel>
+
+        <StyledLabel>
+          {`kcal: ${user.nutritionInfo.kcal.toFixed(0)}kcal`}
+          {'\n\n'}
+          {`proteina: ${user.nutritionInfo.prot.toFixed(0)}g`}
+          {'\n\n'}
+          {`carboidrato ${user.nutritionInfo.carb.toFixed(0)}g`}
+          {'\n\n'}
+          {`gordura: ${user.nutritionInfo.fat.toFixed(0)}g`}
+          {'\n\n'}
+          {`sodio: ${(user.nutritionInfo.sodium * 1000).toFixed(0)}mg`}
+          {'\n\n'}
+          {`fibra: ${user.nutritionInfo.fiber}g`}
+        </StyledLabel>
+
+        <StyledLabel>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, Lorem Ipsum is simply dummy text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s
+        </StyledLabel>
+
+        <StyledWrapperButtonSubmit>
+          <Button title="Continuar" onPress={() => navigate('MenuProfile')} />
+        </StyledWrapperButtonSubmit>
+      </StyledScroll>
+    </Background>
+  );
+};
+
+export default ResultGoal;
