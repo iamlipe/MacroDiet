68e5b8345a455bb2566cbd9c79a84f8709b92203
felipe <felipe_lima10@icloud.com>
2023-04-13 00:37:34 -0300

    feat: add favoritefoods in store

--- src/stores/food.ts
+++ src/stores/food.ts
@@ -4,11 +4,15 @@ import { create } from 'zustand';
 type State = {
   foods: IFood[] | null;
   setFoods: (foods: IFood[]) => void;
+  favoriteFoods: IFood[] | null;
+  setFavoriteFoods: (favoriteFoods: IFood[]) => void;
 };
 
 const useFoodStore = create<State>(set => ({
   foods: null,
   setFoods: (foods: IFood[]) => set(() => ({ foods })),
+  favoriteFoods: null,
+  setFavoriteFoods: (favoriteFoods: IFood[]) => set(() => ({ favoriteFoods })),
 }));
 
 export default useFoodStore;
